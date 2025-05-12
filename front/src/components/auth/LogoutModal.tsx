0c71f62f1ca1c177cdbd0a54d724a63cb4ee01bd
Mohit Soni <developer_mohitsoni@outlook.com>
2025-05-12 16:07:02 +0530

    feat: add LoginModal and LogoutModal components for user authentication

---
+++ front/src/components/auth/LogoutModal.tsx
@@ -0,0 +1,44 @@
+"use client";
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle
+} from "@/components/ui/alert-dialog";
+import { signOut } from "next-auth/react";
+import type { Dispatch, SetStateAction } from "react";
+
+export default function LogoutModal({
+	open,
+	setOpen
+}: {
+	open: boolean;
+	setOpen: Dispatch<SetStateAction<boolean>>;
+}) {
+	const handleLogout = () => {
+		signOut({
+			callbackUrl: "/",
+			redirect: true
+		});
+	};
+	return (
+		<AlertDialog open={open} onOpenChange={setOpen}>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+					<AlertDialogDescription>
+						This action will expire your session.
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel>Cancel</AlertDialogCancel>
+					<AlertDialogAction onClick={handleLogout}>Continue</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
+}
---
+++ front/src/components/auth/ProfileMenu.tsx
@@ -0,0 +1,41 @@
+"use client";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+import UserAvatar from "../common/UserAvatar";
+const LogoutModal = dynamic(() => import("../auth/LogoutModal"));
+
+export default function ProfileMenu({
+	image,
+	name
+}: {
+	image?: string;
+	name: string;
+}) {
+	const [logoutOpen, setLogoutOpen] = useState(false);
+	return (
+		<>
+			{logoutOpen && <LogoutModal open={logoutOpen} setOpen={setLogoutOpen} />}
+			<DropdownMenu>
+				<DropdownMenuTrigger>
+					<UserAvatar name={name} image={image} />
+				</DropdownMenuTrigger>
+				<DropdownMenuContent>
+					<DropdownMenuLabel>My Account</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem>Profile</DropdownMenuItem>
+					<DropdownMenuItem onClick={() => setLogoutOpen(true)}>
+						Logout
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</>
+	);
+}
